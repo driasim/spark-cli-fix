4b2d835a9e851584cdeccae77325239fcf12868c
vibeforge1111 <vibeforge1111@gmail.com>
2026-05-15 18:19:44 +0400

    Bump public installer to r7

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Prefix = "$HOME\.spark",
     [string]$Source = "https://github.com/vibeforge1111/spark-cli",
-    [string]$Ref = "805f45eec7ad213424e05284bb3a83eff3dfd33b",
+    [string]$Ref = "45a6d10838e62d2e9905e9749c26e113c8d6bbed",
     [string]$NodeVersion = "22.18.0",
     [string]$PythonVersion = "3.11",
     [string]$UvVersion = "0.11.7",
@@ -30,7 +30,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$SparkCliReleaseName = "spark-cli-public-installer-2026-05-15-r6"
+$SparkCliReleaseName = "spark-cli-public-installer-2026-05-15-r7"
 $RefWasProvided = $PSBoundParameters.ContainsKey("Ref")
 $Script:InstallLockDir = ""
 $Script:PythonExe = ""
